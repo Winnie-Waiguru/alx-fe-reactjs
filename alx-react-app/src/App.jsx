4a4886b4795070f43f046f132884a1bacd59e6a4
PC <winniewaiguru@gmail.com>
2025-07-10 11:21:14 +0300

    Added WelcomeMessage.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
