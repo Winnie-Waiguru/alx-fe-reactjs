2b6a96097863d4df15e4b2855abb9bdd7011df05
PC <winniewaiguru@gmail.com>
2025-07-17 06:58:55 +0300

    Created a simple Counter app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,9 @@
-import UserProfile from "./components/UserProfile";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
+      <Counter />
     </>
   );
 }
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p style={{ fontSize: "32px", color: "greenyellow" }}>
+        Current Count: `${count}`
+      </p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
