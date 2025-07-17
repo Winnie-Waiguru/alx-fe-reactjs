207f5d99dcdd3f1d181894a3fe1162ca6187817d
PC <winniewaiguru@gmail.com>
2025-07-17 06:32:46 +0300

    Inline styling for header mainContent userProfile & Footer

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ fontSize: "20px", color: "navy" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
