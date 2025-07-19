427e510c3b52eb82b7b947266db9ee14081aa30a
PC <winniewaiguru@gmail.com>
2025-07-19 07:24:07 +0300

    Added my-company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 import Layout from "./components/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -22,6 +23,7 @@ const Routes = createBrowserRouter([
 function App() {
   return (
     <>
+      <Navbar />
       <RouterProvider router={Routes} />
     </>
   );
