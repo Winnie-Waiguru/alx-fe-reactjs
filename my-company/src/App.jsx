5b5d094f444097e9d68c6dc9b042483909b96524
PC <winniewaiguru@gmail.com>
2025-07-19 07:21:34 +0300

    Added my-company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Children } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Layout from "./components/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Routes = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/services", element: <Services /> },
-  { path: "/contact", element: <Contact /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: true, element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/services", element: <Services /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
 ]);
 
 function App() {
