01fd0eada8d9db52aa3180da0d58dde036fd1f7b
PC <winniewaiguru@gmail.com>
2025-08-03 17:16:07 +0300

    Enhancing User Interface &  Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,16 +2,16 @@ import axios from "axios";
 const searchApiUrl = "https://api.github.com/search/users?q=";
 const usersApiUrl = "https://api.github.com/users/";
 
-export async function fetchUserData(userName, location = "", repo = 0) {
+export async function fetchUserData(userName, location = "", minRepos = 0) {
   let query = encodeURIComponent(userName);
   try {
     // Add location filter if provided
     if (location.trim() !== "") {
       query += `+location:${encodeURIComponent(location)}`;
     }
-    // Add repo filter if a positive number
-    if (repo && !isNaN(repo) && Number(repo) > 0) {
-      query += `+repos:>${encodeURIComponent(repo)}`;
+    // Add minRepos filter if a positive number
+    if (minRepos && !isNaN(minRepos) && Number(minRepos) > 0) {
+      query += `+repos:>${encodeURIComponent(minRepos)}`;
     }
 
     // First request: search users
@@ -23,13 +23,11 @@ export async function fetchUserData(userName, location = "", repo = 0) {
     }
 
     let userNamesArray = users.map((user) => user.login);
-    let topFiveUserNamesArray = userNamesArray.slice(0, 6);
-    console.log(topFiveUserNamesArray); //testing purposes
+    let topFiveUserNamesArray = userNamesArray.slice(0, 6); //Reduce the number of names obtained
 
     let userDetailsPromises = topFiveUserNamesArray.map((username) =>
       axios.get(`${usersApiUrl}${username}`)
     );
-    console.log(userDetailsPromises); //testing purposes
 
     let promiseFeedback = await Promise.all(userDetailsPromises);
     const result = promiseFeedback.map((detailPromise) => detailPromise.data);
