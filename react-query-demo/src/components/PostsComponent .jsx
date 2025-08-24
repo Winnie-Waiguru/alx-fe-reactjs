14d1b772ba759fca053945bcb9110254ee692842
PC <winniewaiguru@gmail.com>
2025-08-24 06:53:22 +0300

    Added Advanced Data Handling with React Query

---
+++ react-query-demo/src/components/PostsComponent .jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10"
+  );
+  return response.data;
+};
+
+function PostsComponent() {
+  const { data, refetch, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"], //unique key for this query
+    queryFn: fetchPosts, // function that fetches the posts
+  });
+
+  const handleRefetch = () => {
+    refetch(); //calls refetch to trigger new data fetch
+  };
+
+  if (isLoading) return <div>Loading posts...</div>;
+
+  if (isError) return <div>Error: {error.message}</div>;
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={handleRefetch}>Refetch data</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
