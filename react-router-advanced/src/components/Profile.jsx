5dbc4206ec43a7c8035920f5fad536eeca8d4294
PC <winniewaiguru@gmail.com>
2025-08-24 14:48:42 +0300

    Added Added Advanced Routing in React with React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,22 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
   return (
     <div>
       <h1>User Profile</h1>
+
       <nav>
         <Link to="details">Profile Details</Link> |{" "}
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet />
+
+      {/* Nested Routes go here */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
